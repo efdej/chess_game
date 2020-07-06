72910cb57a7a6b460ba44d81c5da713c25a0685a
d4n <Dan3436@hotmail.com>
2020-07-06 17:39:12 -0500

    Add project files.

---
+++ RepoTesting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RepoTesting
+{
+    class Program
+    {
+        static void Main()
+        {
+            var test = new TestClass.TestClass();
+            Console.WriteLine(test.Hello());
+        }
+    }
+}
---
+++ RepoTesting/TestClass/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepoTesting.TestClass
+{
+    public class TestClass
+    {
+        public string Hello()
+        {
+            return "Hello World!";
+        }
+    }
+}
