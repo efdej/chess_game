1703b08909937297f16408be4a87f98e7fe36746
d4n <dan3436@hotmail.com>
2020-10-01 14:05:25 -0500

    Cleanup and fix warnings

--- Chess/Main.cs
+++ Chess/Main.cs
@@ -1,78 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Chess
 {
     public partial class Chess : Form
     {
-        private bool GameLoaded = false;
-        private Timer timer1 = new Timer();
-
-        private Point downPoint;
-        private bool moved;
+        // This is used to store the CellBounds together with the Cell position
+        // so that we can find the Cell position later (after releasing mouse).
+        readonly private Dictionary<TableLayoutPanelCellPosition, Rectangle> dict = new Dictionary<TableLayoutPanelCellPosition, Rectangle>();
+        readonly private Color[,] _bgColors = new Color[8, 8];
+        // private bool _gameLoaded = false;
+        private Point _downPoint;
+        private bool _moved;
 
         public Chess()
         {
             InitializeComponent();
+            //This will prevent flicker
+            typeof(TableLayoutPanel).GetProperty("DoubleBuffered", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue(BoardPanel, true, null);
         }
 
         private void Chess_Load(object sender, EventArgs e)
         {
 
-            foreach (Control c in this.Controls)
-            {
-                if (c is PictureBox)
-                {
-                    //c.Click += PiecesClick;
-                    c.MouseDown += Pieces_MouseDown;
-                    c.MouseMove += Pieces_MouseMove;
-                    c.MouseUp += Pieces_MouseUp;
-
-                }
-
-            }
-
         }
 
-        //This is used to store the CellBounds together with the Cell position
-        //so that we can find the Cell position later (after releasing mouse).
-        Dictionary<TableLayoutPanelCellPosition, Rectangle> dict = new Dictionary<TableLayoutPanelCellPosition, Rectangle>();
+        private void Pieces_Click(object sender, EventArgs e)
+        {
+            _bgColors[0, 2] = Color.Yellow;
+            _bgColors[0, 3] = Color.Yellow;
+            BoardPanel.Refresh();
+        }
+
         //MouseDown event handler for all your controls (on the tableLayoutPanel1)
         private void Pieces_MouseDown(object sender, MouseEventArgs e)
         {
-            Control button = sender as Control;
-            button.Parent = this;
-            button.BringToFront();
-            downPoint = e.Location;
+            Control piece = sender as Control;
+            piece.Parent = this;
+            piece.BringToFront();
+            _downPoint = e.Location;
         }
+
         //MouseMove event handler for all your controls (on the tableLayoutPanel1)
         private void Pieces_MouseMove(object sender, MouseEventArgs e)
         {
-            Control button = sender as Control;
+            Control piece = sender as Control;
             if (e.Button == MouseButtons.Left)
             {
-                button.Left += e.X - downPoint.X;
-                button.Top += e.Y - downPoint.Y;
-                moved = true;
+                piece.Left += e.X - _downPoint.X;
+                piece.Top += e.Y - _downPoint.Y;
+                _moved = true;
                 BoardPanel.Invalidate();
             }
         }
         //MouseUp event handler for all your controls (on the tableLayoutPanel1)
         private void Pieces_MouseUp(object sender, MouseEventArgs e)
         {
-            Control button = sender as Control;
-            if (moved)
+            Control piece = sender as Control;
+            if (_moved)
             {
-                SetControl(button, e.Location);
-                button.Parent = BoardPanel;
-                moved = false;
+                SetControl(piece, e.Location);
+                piece.Parent = BoardPanel;
+                _moved = false;
             }
         }
         //This is used to set the control on the tableLayoutPanel after releasing mouse
@@ -89,6 +81,8 @@ namespace Chess
         private void BoardPanel_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
         {
             dict[new TableLayoutPanelCellPosition(e.Column, e.Row)] = e.CellBounds;
+            /*
+            
             if (moved)
             {
                 if (e.CellBounds.Contains(BoardPanel.PointToClient(MousePosition)))
@@ -96,27 +90,23 @@ namespace Chess
                     e.Graphics.FillRectangle(Brushes.Yellow, e.CellBounds);
                 }
             }
+            */
+            using (var b = new SolidBrush(_bgColors[e.Column, e.Row]))
+            {
+
+                e.Graphics.FillRectangle(b, e.CellBounds);
+            }
         }
 
-        private void PiecesClick(object sender, EventArgs e)
-        {
-
-        }
-
-        private void ShowBoard(bool Show)
-        {
-            BoardPanel.Visible = Show;
-        }
-
-        private void menuItemNewGame_Click(object sender, EventArgs e)
-        {
-            ShowBoard(true);
-            GameLoaded = true;
-        }
-
-        private void menuItemExit_Click(object sender, EventArgs e)
+        private void MenuItemExit_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void MenuItemNewGame_Click(object sender, EventArgs e)
+        {
+            BoardPanel.Show();
+            // _gameLoaded = true;
         }
     }
 }
